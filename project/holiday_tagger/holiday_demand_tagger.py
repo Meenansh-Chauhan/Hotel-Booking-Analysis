34da6ce58bf2919b623b175feaf0073f0aed8a9b
Meenansh <chauhanmeenansh@gmail.com>
2026-07-01 12:03:14 +0530

    powerBI Dashboard initialized

--- project/holiday_tagger/holiday_demand_tagger.py
+++ project/holiday_tagger/holiday_demand_tagger.py
@@ -10,25 +10,56 @@ YEAR = 2024
 COUNTRY_CODE = "IN"
 
 url = (
-    f"https://date.nager.at/api/v3/PublicHolidays/"
+    f"https://date.nager.at/api/v3/PublicHolidays/."
     f"{YEAR}/{COUNTRY_CODE}"
 )
 
 try:
-
     response = requests.get(
         url,
-        timeout=10
+        timeout=10,
+        headers={
+            "User-Agent": "Mozilla/5.0"
+        }
     )
+
+    print("Status Code:", response.status_code)
+    print("Content-Type:", response.headers.get("Content-Type"))
+    print("\nResponse Preview:\n")
+    print(response.text[:500])      # Print first 500 characters
 
     response.raise_for_status()
 
     holidays = response.json()
 
-    print(
-        f"Successfully fetched "
-        f"{len(holidays)} holidays."
+    print(f"Successfully fetched {len(holidays)} holidays.")
+        
+        # Convert API response to DataFrame
+    holiday_df = pd.DataFrame(holidays)
+
+    # Keep only useful columns
+    holiday_df = holiday_df[
+        [
+            "date",
+            "localName",
+            "name",
+            "countryCode",
+            "fixed",
+            "global",
+            "types"
+        ]
+    ]
+
+    # Convert date column to datetime
+    holiday_df["date"] = pd.to_datetime(holiday_df["date"])
+
+    # Save as CSVs
+    holiday_df.to_csv(
+        "../data/processed/holiday_calendar.csv",
+        index=False
     )
+
+    print("holiday_calendar.csv saved successfully.")
 
 except requests.exceptions.RequestException as e:
 
